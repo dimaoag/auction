fc7bbb6cf6890042096fe24f831d21cf6f8dd991
Dmytro Orikhovskyi <d>
2020-03-11 21:59:59 +0200

    add update JoinConfirmationSender

--- api/src/Auth/Service/JoinConfirmationSender.php
+++ api/src/Auth/Service/JoinConfirmationSender.php
@@ -6,6 +6,7 @@ namespace App\Auth\Service;
 
 use App\Auth\Entity\User\Token;
 use App\Auth\Entity\User\Email;
+use App\Frontend\FrontendUrlGenerator;
 use RuntimeException;
 use Swift_Mailer;
 use Swift_Message;
@@ -13,22 +14,21 @@ use Swift_Message;
 class JoinConfirmationSender
 {
     private Swift_Mailer $mailer;
-    private array $from;
+    private FrontendUrlGenerator $frontend;
 
-    public function __construct(Swift_Mailer $mailer, array $from)
+    public function __construct(Swift_Mailer $mailer, FrontendUrlGenerator $frontend)
     {
         $this->mailer = $mailer;
-        $this->from = $from;
+        $this->frontend = $frontend;
     }
 
     public function send(Email $email, Token $token): void
     {
         $message = (new Swift_Message('Join Confirmation'))
-            ->setFrom($this->from)
             ->setTo($email->getValue())
-            ->setBody('/join/confirm?' . http_build_query([
-                    'token' => $token->getValue(),
-                ]));
+            ->setBody($this->frontend->generate('join/confirm', [
+                'token' => $token->getValue(),
+            ]));
 
         if ($this->mailer->send($message) === 0) {
             throw new RuntimeException('Unable to send email.');
