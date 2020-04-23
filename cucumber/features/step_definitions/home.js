13e80d2064829945967cdac81f294603b2efa800
Dmytro Orikhovskyi <d>
2020-04-23 22:57:40 +0300

    create browser in hooks

--- cucumber/features/step_definitions/home.js
+++ cucumber/features/step_definitions/home.js
@@ -1,11 +1,9 @@
 const { Given, When, Then } = require('cucumber')
 
-Given('I am a guest user', function () {
-  return 'pending'
-})
+Given('I am a guest user', function () {})
 
-When('I open home page', function () {
-  return 'pending'
+When('I open home page', async function () {
+  return await this.page.goto('http://gateway:8080')
 })
 
 Then('I see welcome block', function () {
