3010500a1e27a5c11a40ff59d0dc415a0c5f0b20
Dmytro Orikhovskyi <d>
2021-02-03 21:53:23 +0200

    api FeaturesMonologProcessor

---
+++ api/src/FeatureToggle/Test/Unit/FeaturesMonologProcessorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FeatureToggle\Test\Unit;
+
+use App\FeatureToggle\FeaturesContext;
+use App\FeatureToggle\FeaturesMonologProcessor;
+use PHPUnit\Framework\TestCase;
+
+class FeaturesMonologProcessorTest extends TestCase
+{
+    public function testProcess(): void
+    {
+        $context = $this->createStub(FeaturesContext::class);
+        $context->method('getAllEnabled')->willReturn($source = ['ONE', 'TWO']);
+
+        $processor = new FeaturesMonologProcessor($context);
+
+        $result = $processor([
+            'message' => 'Message'
+        ]);
+
+        self::assertEquals([
+            'message' => 'Message',
+            'extra' => [
+                'features' => $source
+            ],
+        ], $result);
+    }
+}
