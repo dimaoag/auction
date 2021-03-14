68f061a2c74cda4b3bedd583a988f5e93bc41a80
Dmytro Orikhovskyi <d>
2021-03-14 17:30:06 +0200

    add react-router

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './App.css'
-import Home from '../Home'
 import { FeaturesProvider } from '../FeatureToggle'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from '../Home'
+import { NotFound } from '../Error'
 
 function App({ features }) {
   return (
     <FeaturesProvider features={features}>
-      <div className="app">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </FeaturesProvider>
   )
 }
