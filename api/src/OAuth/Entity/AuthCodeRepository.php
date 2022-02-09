eecdc4ef88a578b8fd86cc888129cad6de99fc67
Dmytro Orikhovskyi <dmytro.orikhovskyi@lembergsolutions.com>
2022-02-09 21:56:26 +0200

    ClearExpiredCommand

--- api/src/OAuth/Entity/AuthCodeRepository.php
+++ api/src/OAuth/Entity/AuthCodeRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\OAuth\Entity;
 
+use DateTimeImmutable;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
@@ -56,6 +57,24 @@ final class AuthCodeRepository implements AuthCodeRepositoryInterface
     public function isAuthCodeRevoked($codeId): bool
     {
         return !$this->exists($codeId);
+    }
+
+    public function removeAllForUser(string $userId): void
+    {
+        $this->em->createQueryBuilder()
+            ->delete(AuthCode::class, 'ac')
+            ->andWhere('ac.userIdentifier = :user_id')
+            ->setParameter(':user_id', $userId)
+            ->getQuery()->execute();
+    }
+
+    public function removeAllExpired(DateTimeImmutable $now): void
+    {
+        $this->em->createQueryBuilder()
+            ->delete(AuthCode::class, 'ac')
+            ->andWhere('ac.expiryDateTime < :date')
+            ->setParameter(':date', $now->format(DATE_ATOM))
+            ->getQuery()->execute();
     }
 
     private function exists(string $id): bool
